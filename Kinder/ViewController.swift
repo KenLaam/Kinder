27835c7d7c8e122a9c1345ed905f3c00225313e0
Ken Lâm <hoangvu23108@gmail.com>
2020-09-04 22:31:03 +0700

    Initialize Project

---
+++ Kinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kinder
+//
+//  Created by Ken Lâm on 9/4/20.
+//  Copyright © 2020 KenK11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
